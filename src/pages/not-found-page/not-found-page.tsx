4c87d0ea75afb48981db36a195e5767ed61f47c4
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-09-30 16:38:43 +0300

    разделил интерфейс на компоненты

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,14 +1,20 @@
 import {Link} from 'react-router-dom';
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <div className="page">
-      <h1>Error 404.
-        <br />
-        <small>Page not found</small>
-      </h1>
-      <Link to="/">Go to main page</Link>
-    </div>
+    <>
+      <Header/>
+      <div className="page">
+        <h1>Error 404.
+          <br />
+          <small>Page not found</small>
+        </h1>
+        <Link to="/">Go to main page</Link>
+      </div>
+      <Footer />
+    </>
   );
 }
 
