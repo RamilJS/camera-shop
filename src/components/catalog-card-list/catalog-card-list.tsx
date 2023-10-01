bd900bc40d984393989ad9fc92c7c0e93c6d3477
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-01 14:50:18 +0300

    дробление интерфейса продолжение

---
+++ src/components/catalog-card-list/catalog-card-list.tsx
@@ -0,0 +1,19 @@
+import CatalogCard from '../catalog-card/catalog-card';
+
+function CatalogCardList(): JSX.Element {
+  return (
+    <div className="cards catalog__cards">
+      <CatalogCard />
+      <CatalogCard />
+      <CatalogCard />
+      <CatalogCard />
+      <CatalogCard />
+      <CatalogCard />
+      <CatalogCard />
+      <CatalogCard />
+      <CatalogCard />
+    </div>
+  );
+}
+
+export default CatalogCardList;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,17 +1,20 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 function Footer(): JSX.Element {
   return (
     <footer className="footer">
       <div className="container">
         <div className="footer__info">
-          <a
+          <Link
             className="footer__logo"
-            href="index.html"
+            to={AppRoute.Main}
             aria-label="Переход на главную"
           >
             <svg width={100} height={36} aria-hidden="true">
               <use xlinkHref="#icon-logo-mono" />
             </svg>
-          </a>
+          </Link>
           <p className="footer__description">
             Интернет-магазин фото- и видеотехники
           </p>
@@ -56,24 +59,24 @@ function Footer(): JSX.Element {
             <p className="footer__title">Навигация</p>
             <ul className="footer__list">
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to={AppRoute.Main}>
                   Каталог
-                </a>
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to="#">
                   Гарантии
-                </a>
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to="#">
                   Доставка
-                </a>
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to="#">
                   О компании
-                </a>
+                </Link>
               </li>
             </ul>
           </li>
@@ -81,19 +84,19 @@ function Footer(): JSX.Element {
             <p className="footer__title">Ресурсы</p>
             <ul className="footer__list">
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to="#">
                   Курсы операторов
-                </a>
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to="#">
                   Блог
-                </a>
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to="#">
                   Сообщество
-                </a>
+                </Link>
               </li>
             </ul>
           </li>
@@ -101,14 +104,14 @@ function Footer(): JSX.Element {
             <p className="footer__title">Поддержка</p>
             <ul className="footer__list">
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to="#">
                   FAQ
-                </a>
+                </Link>
               </li>
               <li className="footer__item">
-                <a className="link" href="#">
+                <Link className="link" to="#">
                   Задать вопрос
-                </a>
+                </Link>
               </li>
             </ul>
           </li>
