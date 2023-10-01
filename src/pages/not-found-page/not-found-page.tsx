2cfbe994e78c2e1bb5f4c161612a70bb150b9c48
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-02 00:09:16 +0300

    стили для страницы 404

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,18 +1,22 @@
 import {Link} from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import { AppRoute } from '../../const';
+import styles from './not-found-page.module.css';
 
 function NotFoundPage(): JSX.Element {
   return (
     <>
       <Header/>
-      <div className="page">
-        <h1>Error 404.
+      <div className={`page ${styles.notFoundPage}`}>
+        <Helmet>
+          <title>Escape Room: 404</title>
+        </Helmet>
+        <h1 className={styles.title}>Error 404
           <br />
-          <small>Page not found</small>
+          <Link to={AppRoute.Main}>Go to main page</Link>
         </h1>
-        <Link to={AppRoute.Main}>Go to main page</Link>
       </div>
       <Footer />
     </>
