37ab5727bf5714ded37f46f604c6c320462695c6
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-11 23:13:36 +0300

    минус слайсы корзины

--- src/store/basket-data/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { NameSpace } from '../../const';
-import { State } from '../../types/state';
-import { Camera } from '../../types/camera';
-
-export const getBasketSuccessModalStatus = (state: State): boolean => state[NameSpace.BasketData].successModalOpen;
-export const getCameraInBasketModal = (state: State): Camera => state[NameSpace.BasketData].cameraInBasketModal as Camera;
-
--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,10 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { NameSpace } from '../const';
 import { camerasSlice } from './cameras-data/cameras-data';
-import { basketSlice } from './basket-data/basket-data';
+//import { basketSlice } from './basket-data/basket-data';
 
 
 export const rootReducer = combineReducers({
   [NameSpace.CamerasData]: camerasSlice.reducer,
-  [NameSpace.BasketData]: basketSlice.reducer,
+  //[NameSpace.BasketData]: basketSlice.reducer,
 });
