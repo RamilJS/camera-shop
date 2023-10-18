802e5fb641b4da70c5480f7336d4b72735e3ea24
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-18 16:28:29 +0300

    лоадер тест

---
+++ src/components/loader/loader.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Loader from './loader';
+
+describe('Component: Loader', () => {
+
+  it('should render correctly', () => {
+    const expectedText = /Loading/i;
+
+    render(<Loader/>);
+
+    expect(screen.getByText(expectedText)).toBeInTheDocument();
+  });
+});
