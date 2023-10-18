e2e06664147975b3d8625def5c2d365b719d9378
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-19 00:38:31 +0300

    basket-item тест

---
+++ src/components/basket-item/basket-item.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import BasketItem from './basket-item';
+
+describe('Component: Loader', () => {
+
+  it('should render correctly', () => {
+    const articulText = /Артикул:/;
+    const priceText = /Цена:/;
+    const commonPriceText = /Общая цена:/;
+
+    render(<BasketItem/>);
+
+    expect(screen.getByText(articulText)).toBeInTheDocument();
+    expect(screen.getByText(priceText)).toBeInTheDocument();
+    expect(screen.getByText(commonPriceText)).toBeInTheDocument();
+  });
+});
