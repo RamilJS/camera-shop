8422e44fd314a6d4214052cb5d9fab401fa5735d
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-19 18:25:41 +0300

    хок withStore

--- src/utils-test/mock-component.tsx
+++ src/utils-test/mock-component.tsx
@@ -1,6 +1,16 @@
 import { createMemoryHistory, MemoryHistory } from 'history';
 import HistoryRouter from '../hocs/history-router';
 import { HelmetProvider } from 'react-helmet-async';
+import { MockStore, configureMockStore } from '@jedmao/redux-mock-store';
+import MockAdapter from 'axios-mock-adapter';
+import thunk from 'redux-thunk';
+import { ThunkDispatch } from 'redux-thunk';
+import { Action } from 'redux';
+import { Provider } from 'react-redux';
+import { State } from '../types/state';
+import { createAPI } from '../services/api';
+
+export type AppThunkDispatch = ThunkDispatch<State, ReturnType<typeof createAPI>, Action>;
 
 export function withHistory(component: JSX.Element, history?: MemoryHistory) {
   const memoryHistory = history ?? createMemoryHistory();
@@ -13,3 +23,27 @@ export function withHistory(component: JSX.Element, history?: MemoryHistory) {
     </HistoryRouter>
   );
 }
+
+type ComponentWithMockStore = {
+  withStoreComponent: JSX.Element;
+  mockStore: MockStore;
+  mockAxiosAdapter: MockAdapter;
+}
+
+export function withStore(
+  component: JSX.Element,
+  initialState: Partial<State> = {},
+): ComponentWithMockStore {
+  const axios = createAPI();
+  const mockAxiosAdapter = new MockAdapter(axios);
+  const middleware = [thunk.withExtraArgument(axios)];
+  const mockStoreCreator = configureMockStore<State, Action<string>, AppThunkDispatch>(middleware);
+  const mockStore = mockStoreCreator(initialState);
+
+  return ({
+    withStoreComponent: <Provider store={mockStore}>{component}</Provider>,
+    mockStore,
+    mockAxiosAdapter,
+  });
+}
+
