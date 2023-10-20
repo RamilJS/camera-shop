a5b93451bf88bf80a2ede545a829b7776a2673ab
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-20 17:25:42 +0300

    тест список отзывов

---
+++ src/components/review-list-block/review-list-block.test.tsx
@@ -0,0 +1,98 @@
+import { screen, render } from '@testing-library/react';
+import ReviewListBlock from './review-list-block';
+import { withHistory, withStore } from '../../utils-test/mock-component';
+import { makeFakeCamera, makeFakePromoCamera, makeFakeReview } from '../../utils-test/mocks';
+import { NameSpace, Status } from '../../const';
+
+const fakeCamera = makeFakeCamera();
+const mockPromoCamera = [makeFakePromoCamera(), makeFakePromoCamera()];
+const fakeCameras = [makeFakeCamera(), makeFakeCamera(), makeFakeCamera(), makeFakeCamera(), makeFakeCamera()];
+const mockReviews = [makeFakeReview(), makeFakeReview(), makeFakeReview(), makeFakeReview(), makeFakeReview(), makeFakeReview()];
+
+
+describe('Component: ReviewListBlock', () => {
+
+  it('should render text correctly', () => {
+    const showMoreReviewsText = 'Показать больше отзывов';
+
+    const { withStoreComponent } = withStore(<ReviewListBlock/>, {
+      [NameSpace.CamerasData]: {
+        cameras: {
+          data: fakeCameras,
+          status: Status.Unsent,
+        },
+        camera: {
+          product: fakeCamera,
+          isLoading: false,
+          similarCameras: fakeCameras,
+          isSimilarLoading: false,
+          successModalOpen: false,
+        },
+        promoCamera: {
+          data: mockPromoCamera,
+          status: Status.Unsent,
+        },
+        modalCameras: {
+          cameraInBasketModal: fakeCamera,
+        },
+      },
+      [NameSpace.ReviewsData]: {
+        reviews: mockReviews,
+        status: Status.Unsent,
+        postReviewSuccessStatus: false,
+        addReviewModalOpen: false,
+        reviewPostingStatus: Status.Unsent,
+      },
+
+    });
+    const preparedComponent = withHistory(withStoreComponent);
+    render(preparedComponent);
+
+    const button = screen.getByRole('button', { name: /Оставить свой отзыв/i });
+
+    expect(button).toBeInTheDocument();
+    expect(screen.getByText(showMoreReviewsText)).toBeInTheDocument();
+  });
+
+  it('Initial reviews should be displayed', () => {
+    const DEFAULT_RENDERED_REVIEWS_COUNT = 3;
+
+    const { withStoreComponent } = withStore(<ReviewListBlock/>, {
+      [NameSpace.CamerasData]: {
+        cameras: {
+          data: fakeCameras,
+          status: Status.Unsent,
+        },
+        camera: {
+          product: fakeCamera,
+          isLoading: false,
+          similarCameras: fakeCameras,
+          isSimilarLoading: false,
+          successModalOpen: false,
+        },
+        promoCamera: {
+          data: mockPromoCamera,
+          status: Status.Unsent,
+        },
+        modalCameras: {
+          cameraInBasketModal: fakeCamera,
+        },
+      },
+      [NameSpace.ReviewsData]: {
+        reviews: mockReviews,
+        status: Status.Unsent,
+        postReviewSuccessStatus: false,
+        addReviewModalOpen: false,
+        reviewPostingStatus: Status.Unsent,
+      },
+
+    });
+    const preparedComponent = withHistory(withStoreComponent);
+    render(preparedComponent);
+
+    mockReviews.slice(0, DEFAULT_RENDERED_REVIEWS_COUNT).forEach((review) => {
+      const reviewText = screen.getByText(review.review);
+      expect(reviewText).toBeInTheDocument();
+    });
+  });
+});
