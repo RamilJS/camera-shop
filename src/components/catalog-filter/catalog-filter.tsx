479aeeeed6e5f05c394c62853b0d4f740f56b7a3
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-25 16:18:47 +0300

    убрал фильтры по умолчанию

--- src/components/catalog-filter/catalog-filter.tsx
+++ src/components/catalog-filter/catalog-filter.tsx
@@ -29,7 +29,6 @@ function CatalogFilter(): JSX.Element {
               <input
                 type="checkbox"
                 name="photocamera"
-                defaultChecked
               />
               <span className="custom-checkbox__icon" />
               <span className="custom-checkbox__label">
@@ -54,7 +53,6 @@ function CatalogFilter(): JSX.Element {
               <input
                 type="checkbox"
                 name="digital"
-                defaultChecked
               />
               <span className="custom-checkbox__icon" />
               <span className="custom-checkbox__label">Цифровая</span>
@@ -62,7 +60,7 @@ function CatalogFilter(): JSX.Element {
           </div>
           <div className="custom-checkbox catalog-filter__item">
             <label>
-              <input type="checkbox" name="film" disabled />
+              <input type="checkbox" name="film" />
               <span className="custom-checkbox__icon" />
               <span className="custom-checkbox__label">
                 Плёночная
@@ -83,8 +81,6 @@ function CatalogFilter(): JSX.Element {
               <input
                 type="checkbox"
                 name="collection"
-                defaultChecked
-                disabled
               />
               <span className="custom-checkbox__icon" />
               <span className="custom-checkbox__label">
@@ -97,7 +93,7 @@ function CatalogFilter(): JSX.Element {
           <legend className="title title--h5">Уровень</legend>
           <div className="custom-checkbox catalog-filter__item">
             <label>
-              <input type="checkbox" name="zero" defaultChecked />
+              <input type="checkbox" name="zero" />
               <span className="custom-checkbox__icon" />
               <span className="custom-checkbox__label">Нулевой</span>
             </label>
