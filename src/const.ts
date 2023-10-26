e718438468919fb427a95d74403c3eb6cdcd52eb
RamilJS <124208968+RamilJS@users.noreply.github.com>
2023-10-26 17:11:43 +0300

    попытка исправить роутинг

--- src/const.ts
+++ src/const.ts
@@ -9,7 +9,7 @@ export const REVIEWS_TO_RENDER_COUNT = 3;
 export const enum AppRoute {
   Main = '/',
   Cameras = '/cameras/',
-  Product = '/cameras/:id/:tab',
+  Product = '/product/:id/:tab',
   Basket = '/basket',
   NotFound = '/not-found-screen',
   DescriptionTab = 'description',
